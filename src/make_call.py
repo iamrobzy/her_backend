bfa47f12683691838b7b65a8a5b833b48f71aab5
iamrobzy <robertinshanghai@me.com>
2025-05-09 10:51:50 +0200

    outbound call function done

--- src/make_call.py
+++ src/make_call.py
@@ -1,21 +1,38 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 import os
-
+import requests
+import json
 from dotenv import load_dotenv
-from twilio.rest import Client
+load_dotenv() 
 
-load_dotenv()  # take environment variables
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-client = Client(account_sid, auth_token)
+def outbound_call(first_message, prompt):
+    curl_url = "https://api.elevenlabs.io/v1/convai/twilio/outbound_call"
+    curl_headers = {
+        "Xi-Api-Key": os.getenv("ELEVENLABS_API_KEY"),
+        "Content-Type": "application/json",
+    }
 
-call = client.calls.create(
-    url="http://demo.twilio.com/docs/voice.xml",
-    to="+46765851105",
-    from_="+13412991224",
-)
+    curl_data = {
+        "agent_id": os.getenv("AGENT_ID"),
+        "agent_phone_number_id": os.getenv("AGENT_PHONE_NUMBER_ID"),
+        "to_number": os.getenv("TO_NUMBER"),
+        "conversation_initiation_client_data": {
+            "conversation_config_override": {
+                "agent": {
+                    "prompt": {
+                        "prompt": prompt
+                    },
+                    "first_message": first_message
+                }
+            }
+        }
+    }
 
-print(call.sid)
+    curl_response = requests.post(curl_url, headers=curl_headers, data=json.dumps(curl_data))
+    return curl_response.json()
+
+if __name__ == "__main__":
+
+    first_message = "Hey Robert, you are literally the goat! How is the hackathon going?"
+    prompt = "You're an over-the-top, high-energy hypeman whose only mission is to hype someone up like they're about to walk on stage in front of 100,000 screaming fans. Use slang, rhythm, and pure fire energy. No chill allowed. Go all in on motivation, compliments, and wild metaphors. Ready? Hype me up like I'm the main event!"
+    outbound_call(first_message, prompt)
